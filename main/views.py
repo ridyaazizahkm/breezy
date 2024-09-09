ff35404928f9e14556b7d0e1577335bcfe540b96
ridyaazizahkm <ridyazizahh3@gmail.com>
2024-09-09 09:20:45 +0700

    tugas 2 selesai

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name' : 'Alchemist Fragrance - Galleria | Eau de Parfum',
+        'price': '229.000',
+        'description': 'Alchemist Galleria perfume captures the essence of pure femininity, surrounding you with sweet florals that inspire romance and relaxation, making each moment beautifully serene.',
+        'ukuran' : '50 ml',
+        'aroma' : 'Sweet floral',
+        'top_notes' : 'Cape jasmine, plum',
+        'middle_notes' : 'Tuberose, oiellet',
+        'base_notes' : 'Honey, redwood'
+    }
+
+    return render(request, "main.html", context)
